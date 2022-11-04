4b9e1e42c572f3728aedcb5af97a26c4c96c8d08
AlmirDautovic <aalmir.d@gmail.com>
2022-11-04 15:16:10 +0100

    sending respons for get request on default and home routs

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 
 
-app.use((req, res) => {
-    res.send("HELLO WORLD!!!!");
-})
+// app.use((req, res) => {
+//     res.send("TEST, HELLO WORLD!!!!");
+// })
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get('/home', (req, res) => {
+    res.send("This is a Home Page!");
+});
 
 app.listen(3000, () => {
-    console.log("serving on port 3000!")
-})
+    console.log("serving on port 3000!");
+});
