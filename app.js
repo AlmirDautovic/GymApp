22f2afc216c73d923d45160d25b6c1e59e203412
AlmirDautovic <aalmir.d@gmail.com>
2022-11-11 22:42:51 +0100

    installed module ejs-mate which will add content of all pages in boilerplate that i created separately. also created folder with partials that will help to insert content likle footer, navbar and other easier to other pages

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const app = express();
+const ejsMate = require('ejs-mate');
 const path = require('path');
+
+
+const app = express();
 
 app.use('/PicturesLogo', express.static('PicturesLogo'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-
+app.use(express.urlencoded({ extended: true }));
 // app.use((req, res) => {
 //     res.send("TEST, HELLO WORLD!!!!");
 // })
