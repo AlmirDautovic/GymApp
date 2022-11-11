72c8bae7f8b1093a7e1fdc964bfab0ef94502de0
AlmirDautovic <aalmir.d@gmail.com>
2022-11-11 01:08:28 +0100

    connected css stylesheet from public folder and made navbar partial which will be included in every page later in project

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require('path');
+
 app.use('/PicturesLogo', express.static('PicturesLogo'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 // app.use((req, res) => {
 //     res.send("TEST, HELLO WORLD!!!!");
