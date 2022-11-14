9d8d211ca0ab83d3368ddac9ca340bf5fd428008
AlmirDautovic <aalmir.d@gmail.com>
2022-11-14 15:25:34 +0100

    added form with input fields for username and password. created get and post routs for login and created login page where those username and password are displayed

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ const app = express();
 
 app.use('/public', express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/login', (req, res) => {
+    res.send('get request');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    console.log(req.body);
+    // res.send(`Succesfully Loged In: Your username is: ${username} , and password: ${password}`);
+    res.render('login', { username, password });
+});
 
 app.listen(3000, () => {
     console.log("serving on port 3000!");
