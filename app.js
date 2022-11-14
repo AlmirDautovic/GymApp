02b0e5275e796b9cef3a2a8b58f462412d9f15ff
AlmirDautovic <aalmir.d@gmail.com>
2022-11-14 01:30:04 +0100

    images folder, that now contain logo picture, was created inside public folder and fixed css stylesheet a little. targetetr some parts with selectors instead adding more classes

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const ejsMate = require('ejs-mate');
 const path = require('path');
-
-
 const app = express();
 
-app.use('/PicturesLogo', express.static('PicturesLogo'));
+app.use('/public', express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({ extended: true }));
-// app.use((req, res) => {
-//     res.send("TEST, HELLO WORLD!!!!");
-// })
+
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/home', (req, res) => {
-    res.send("This is a Home Page!");
-});
 
 app.listen(3000, () => {
     console.log("serving on port 3000!");
