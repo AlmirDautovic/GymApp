712f392293a2c2bc5d3697dc622acf5fbec57d9f
AlmirDautovic <aalmir.d@gmail.com>
2022-11-29 23:05:30 +0100

    fixed disolaying buttons when changing theme

--- public/javascripts/themeSet.js
+++ public/javascripts/themeSet.js
@@ -1,10 +1,12 @@
 const btn1 = document.getElementById('dark');
-const btn2 = document.getElementById('white');
+var btn2 = document.getElementById('white');
 
 btn1.addEventListener('click', function onClick(event) {
     document.body.style.backgroundColor = "black";
     document.body.style.color = 'white';
     event.target.classList.add('btn-light');
+    // const btn2 = document.getElementById('white');
+    btn2.classList.add('btn-light')
 });
 
 btn2.addEventListener('click', function onClick(event) {
