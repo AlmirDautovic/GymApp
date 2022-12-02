24b5507e23d5f7d4aaa1eba22c0b61eb856b5c92
AlmirDautovic <aalmir.d@gmail.com>
2022-12-02 18:43:56 +0100

    on home page, instead of two buttons, which were used for switching background to dark and light mode, i placed switcher and edited javascript file that make switcher change between dark/light theme

--- public/javascripts/themeSet.js
+++ public/javascripts/themeSet.js
@@ -1,15 +1,28 @@
-const btn1 = document.getElementById('dark');
-var btn2 = document.getElementById('white');
+// const btn1 = document.getElementById('dark');
+// var btn2 = document.getElementById('white');
 
-btn1.addEventListener('click', function onClick(event) {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = 'white';
-    event.target.classList.add('btn-light');
-    btn2.classList.add('btn-light')
-});
+// btn1.addEventListener('click', function onClick(event) {
+//     document.body.style.backgroundColor = "black";
+//     document.body.style.color = 'white';
+//     event.target.classList.add('btn-light');
+//     btn2.classList.add('btn-light')
+// });
 
-btn2.addEventListener('click', function onClick(event) {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = '#7A7A7A';
-    event.target.classList.add('btn-light');
-});
+// btn2.addEventListener('click', function onClick(event) {
+//     document.body.style.backgroundColor = "white";
+//     document.body.style.color = '#7A7A7A';
+//     event.target.classList.add('btn-light');
+// });
+
+var switcher = document.getElementById('switch');
+
+switcher.addEventListener('change', function onChange(event) {
+    let color = document.body.style.backgroundColor;
+    if (color === 'black') {
+        document.body.style.backgroundColor = 'white'
+        document.body.style.color = '#7A7A7A';
+    } else {
+        document.body.style.backgroundColor = 'black'
+        document.body.style.color = 'white';
+    }
+})
