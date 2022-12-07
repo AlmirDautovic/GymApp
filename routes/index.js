46e20bc53225d6236c5778b9b2e5974efc29f2d7
AlmirDautovic <aalmir.d@gmail.com>
2022-12-07 22:08:06 +0100

    excluded id from json response

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.post('/users/index', async (req, res) => {
 router.get('/users/json', async (req, res) => {
     const { id } = req.params
     console.log(id);
-    const users = await User.findById({ id }, { password: 0, __v: 0 });
+    const users = await User.find({}, { password: 0, _id: 0, __v: 0 });
     res.json(users);
 })
 router.get('/users/:id', async (req, res) => {
