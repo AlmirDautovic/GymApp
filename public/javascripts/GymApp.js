1866167aa5b062b3e1016e507e77133e50a5c3ce
AlmirDautovic <aalmir.d@gmail.com>
2022-12-09 23:34:07 +0100

    started vorking on new section on home page.making crud layout for adding css styles

--- public/javascripts/GymApp.js
+++ public/javascripts/GymApp.js
@@ -1,19 +1,3 @@
-// const btn1 = document.getElementById('dark');
-// var btn2 = document.getElementById('white');
-
-// btn1.addEventListener('click', function onClick(event) {
-//     document.body.style.backgroundColor = "black";
-//     document.body.style.color = 'white';
-//     event.target.classList.add('btn-light');
-//     btn2.classList.add('btn-light')
-// });
-
-// btn2.addEventListener('click', function onClick(event) {
-//     document.body.style.backgroundColor = "white";
-//     document.body.style.color = '#7A7A7A';
-//     event.target.classList.add('btn-light');
-// });
-
 var switcher = document.getElementById('switch');
 if (switcher != null) {
     switcher.addEventListener('change', function onChange(event) {
