55395012f9c42eb096f0b84bf232f058508c7253
AlmirDautovic <aalmir.d@gmail.com>
2022-12-15 16:22:25 +0100

    chenget get json function to use low lvl ajax instead of jQuery

--- public/javascripts/GymApp.js
+++ public/javascripts/GymApp.js
@@ -12,50 +12,50 @@ if (switcher != null) {
     })
 };
 
-// function loadJson(path, succes, error) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === XMLHttpRequest.DONE) {
-//             if (xhr.status === 200) {
-//                 succes(JSON.parse(xhr.responseText));
-//             } else {
-//                 if (error)
-//                     error(xhr);
-//             }
-//         }
-//     };
-//     xhr.open('GET', path, true);
-//     xhr.send();
-// };
-
-// var nameList = '';
-// loadJson('http://localhost:3000/users/json', function (users) {
-//     for (let user of users) {
-//         let name = user.username;
-//         console.log(name)
-//         nameList += name
-//     };
-// },
-//     function (xhr) { console.error(xhr) });
-
-
-// document.getElementById('show').addEventListener('click', function () {
-//     document.getElementById('list').innerHTML += nameList;
-// });
-
-$(document).ready(function () {
-
-    $('#show').click(function () {
-        var list = document.getElementById('list');
-        var userId = document.getElementById('userId').value;
-        $.getJSON('http://localhost:3000/users/json?id=' + userId, function (users) {
-            users.forEach(user => {
-                const li = document.createElement('li');
-                (li).append(user.username);
-                list.appendChild(li);
-            });
-        });
-        $(list).empty()
+function loadJson(path, succes, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                succes(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open('GET', path, true);
+    xhr.send();
+};
+document.getElementById('show').addEventListener('click', function () {
+    var list = document.getElementById('list');
+    list.innerHTML = ''
+    var listTitle = document.getElementById('listTitle').innerHTML = "Other users:"
+    var userId = document.getElementById('userId').value;
+    loadJson('http://localhost:3000/users/json?=' + userId, function (users) {
+        for (let user of users) {
+            var li = document.createElement('li');
+            li.append(user.username);
+            list.appendChild(li);
+        }
     });
 });
+
+
+
+// $(document).ready(function () {
+
+//     $('#show').click(function () {
+//         var list = document.getElementById('list');
+//         var userId = document.getElementById('userId').value;
+//         $(list).empty()
+//         $.getJSON('http://localhost:3000/users/json?id=' + userId, function (users) {
+//             users.forEach(user => {
+//                 const li = document.createElement('li');
+//                 (li).append(user.username);
+//                 list.appendChild(li);
+//             });
+//         });
+//     });
+// });
 
