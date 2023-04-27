54c85e4c0ecc3554746e2e7ef5b3904aded51706
AlmirDautovic <aalmir.d@gmail.com>
2023-04-27 23:38:31 +0200

    cleaned GymApp.js file.removed unnecesarry sode, changed code to use less functions and removed unnecessary functions

--- public/javascripts/GymApp.js
+++ public/javascripts/GymApp.js
@@ -1,7 +1,5 @@
 
-//code for changing to dark or white background on home page:
-
-var homeThemeSwitcher = document.getElementById('switch');
+let homeThemeSwitcher = document.getElementById('switch');
 if (homeThemeSwitcher != null) {
     homeThemeSwitcher.addEventListener('change', function onChange(event) {
         let color = document.body.style.backgroundColor;
@@ -16,10 +14,9 @@ if (homeThemeSwitcher != null) {
 };
 
 
-// Function that is used on single user page (view user details) to show other users:
 
 function loadJson(path, succes, error) {
-    var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
@@ -33,19 +30,19 @@ function loadJson(path, succes, error) {
     xhr.open('GET', path, true);
     xhr.send();
 };
-var showOtherUsersButton = document.getElementById('show');
+
+let showOtherUsersButton = document.getElementById('show');
 if (showOtherUsersButton != null) {
     showOtherUsersButton.addEventListener('click', function () {
-        var list = document.getElementById('list');
-        list.innerHTML = ''
-        var listTitle = document.getElementById('listTitle').innerHTML = "Other registered users:"
-        var userId = document.getElementById('userId').value;
+        let list = document.getElementById('list');
+        list.innerHTML = '';
+        let userId = document.getElementById('userId').value;
         loadJson('http://localhost:3000/users/json?=' + userId, function (users) {
             for (let user of users) {
                 if (user._id == userId) {
                     continue;
                 }
-                var li = document.createElement('li');
+                let li = document.createElement('li');
                 li.append(user.username);
                 list.appendChild(li);
             }
@@ -54,54 +51,24 @@ if (showOtherUsersButton != null) {
 }
 
 
-// Function for dinamically display content on user page, without refreshing after deleting user
-
-function deleteUser(id) {
-    var element = document.querySelector('button[class="page-link active"]');
-    var xhr = new XMLHttpRequest();
-    xhr.open('DELETE', 'http://localhost:3000/users/' + id, true);
-    xhr.onload = function () {
-        if (xhr.readyState == 4 && xhr.status == '200') {
-            createUsersListWithPaginationHtml(element);
-        }
-        else {
-            console.log('ERROR!', error)
-        }
-    }
-    xhr.send();
+function deleteUserOnUsersPage(element) {
+    let activeButtonNumber = document.querySelector('button[class="page-link active"]');
+    let id = element.value;
+    axios.delete('http://localhost:3000/users/' + id)
+        .then(res => {
+            createUsersListWithPaginationHtml(activeButtonNumber)
+        })
+        .catch(err => console.log(err));
 };
 
-function createUsersPageContent() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:3000/users/json', true);
 
-    xhr.onload = function () {
-        var users = JSON.parse(xhr.responseText);
-        var h1 = document.getElementById('allusers');
-        var ul = document.getElementById('userList');
-        if (xhr.readyState == 4 && xhr.status == '200') {
-            ul.innerHTML = '';
-            h1.innerText = "List of all users:"
-            ul.innerHTML = getHtmlForListOfUsers(users)
-        } else {
-            console.error(users);
-        }
-    }
-    xhr.send();
-}
-
-function deleteUserOnUsersPage(element) {
-    deleteUser(element.value);
-}
-
-// delete single user on view user details page
-
-function removeSelectedUser(id) {
-    var xhr = new XMLHttpRequest();
+function removeSelectedUser(element) {
+    let id = element.value;
+    let xhr = new XMLHttpRequest();
     xhr.open("DELETE", 'http://localhost:3000/users/' + id, true);
     xhr.onload = function () {
         if (xhr.readyState == 4 && xhr.status == '200') {
-            redirectAfterDelete();
+            redirectPage();
         }
         else {
             console.error(error);
@@ -110,58 +77,30 @@ function removeSelectedUser(id) {
     xhr.send(null);
 }
 
-function redirectAfterDelete() {
-    var url = "http://localhost:3000/";
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true)
-    xhr.onload = function () {
-        if (xhr.readyState == 4 && xhr.status == "200") {
-            redirectPage();
-        } else {
-            console.error(users);
-        }
-    }
-    xhr.send(null);
-}
-
 function redirectPage(queryString = "") {
     let baseUrl = window.location.origin;
-    window.location.replace(baseUrl + '/users' + queryString); // because it is impossible to redirect page with ajax req i used this 2 lines
-    //of code to redirect with client side
-    console.log(window.location.origin)
+    window.location.replace(baseUrl + '/users' + queryString);
 }
 
-function deleteSpecificUserFromDetailsPage(element) {
-    removeSelectedUser(element.value);
-}
 
-// Function for dimanically display content on user page after selecting user to: all, active or inactive
 
-function gettingUsersByStatus(value) {
-    var h1 = document.getElementById('allusers');
-    var alertContent = document.getElementById('searchAlert');
+function displayingUsersBasedOnStatusValue(element) {
+    let status = element.value;
+    let h1 = document.getElementById('allusers');
+    let alertContent = document.getElementById('searchAlert');
 
-    var userListElement = document.getElementById('userList');
-    var displayNumbers = document.getElementById('pagination');
-    userListElement.innerHTML = '';
-    displayNumbers.innerHTML = '';
-
-    axios.get('http://localhost:3000/pagination', { params: { status: value } })
+    axios.get('http://localhost:3000/pagination', { params: { status: status } })
         .then(res => {
             let role = res.data.role;
-            var users = res.data.users.results;
+            let users = res.data.users.results;
             alertContent.innerHTML = '';
             h1.innerText = "List of all users:"
-            userListElement.innerHTML = getHtmlForListOfUsers(users, role);
-            displayNumbers.innerHTML = createPaginationButtons(res);
-            addActiveClassForButtons(res);
-
+            createUsersPageContentWithPagination(users, role, res)
         })
-        .catch(err => console.log(err))
+        .catch(err => console.log(err));
 }
 
 
-//Function for dinamically create content after deleting user on users page, or selecting users by their status on users page
 function getHtmlForListOfUsers(users, role) {
     var content = '';
     for (let user of users) {
@@ -173,7 +112,7 @@ function getHtmlForListOfUsers(users, role) {
             active = 'checked';
         }
         if (role === 'admin') {
-            rolePrivilege = '=';
+            rolePrivilege = '';
         }
         content +=
             '<li>' +
@@ -204,25 +143,19 @@ function getHtmlForListOfUsers(users, role) {
     return content;
 }
 
-function displayingUsersBasedOnStatusValue(selectValue) {
-    var value = selectValue.value;
-    gettingUsersByStatus(value);
-}
 
-// gym equipment page
-
-function postGymItem() {
-    var url = "http://localhost:3000/gymequipment";
-    var item = {};
+function postAndDisplayGymItems() {
+    let url = "http://localhost:3000/gymequipment";
+    let item = {};
     item.item_name = document.getElementById('item_name').value;
-    // item.image_url = document.getElementById('image_url').value;
-    var json = JSON.stringify(item);
-    var xhr = new XMLHttpRequest();
+    let json = JSON.stringify(item);
+    let xhr = new XMLHttpRequest();
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.onload = function () {
-        var items = JSON.parse(xhr.responseText);
+        let items = JSON.parse(xhr.responseText);
         if (xhr.readyState == 4 && xhr.status == "201") {
+            document.getElementById('item_name').value = ''
             getGymItems();
         } else {
             console.error(items);
@@ -232,12 +165,12 @@ function postGymItem() {
 };
 
 function getGymItems() {
-    var url = "http://localhost:3000/gymitem";
-    var xhr = new XMLHttpRequest();
+    let url = "http://localhost:3000/gymitem";
+    let xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.onload = function () {
-        var items = JSON.parse(xhr.responseText);
-        var itemList = document.getElementById('orderedList');
+        let items = JSON.parse(xhr.responseText);
+        let itemList = document.getElementById('orderedList');
         if (xhr.readyState == 4 && xhr.status == '200') {
             itemList.innerHTML = createListOfGymItems(items);
         } else {
@@ -248,7 +181,7 @@ function getGymItems() {
 }
 
 function createListOfGymItems(items) {
-    var content = '';
+    let content = '';
     for (let item of items) {
         content +=
             '<li>' +
@@ -260,45 +193,37 @@ function createListOfGymItems(items) {
     return content;
 }
 
-function displayGymItems() {
-    postGymItem();
-    document.getElementById('item_name').value = ''
-}
 
 
-// displaying current date in blog post form
 if (document.getElementById("date") != null) {
-    var date = new Date();
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
 
     if (month < 10) month = "0" + month;
     if (day < 10) day = "0" + day;
 
-    var today = year + "-" + month + "-" + day;
+    let today = year + "-" + month + "-" + day;
     document.getElementById("date").value = today;
 };
 
 
-// search user option:
-var searchedName;
+
 function getSearchResultsForUsers() {
-    var alertContent = document.getElementById('searchAlert');
-    searchedName = document.getElementById('search_input').value;
-    var userListElement = document.getElementById('userList');
+    let alertContent = document.getElementById('searchAlert');
+    let searchedName = document.getElementById('search_input').value;
+    let userListElement = document.getElementById('userList');
     userListElement.innerHTML = '';
-    var displayNumbers = document.getElementById('pagination');
+    let displayNumbers = document.getElementById('pagination');
     displayNumbers.innerHTML = '';
     alertContent.innerHTML = '';
     axios.get('http://localhost:3000/pagination', { params: { username: searchedName } })
         .then(res => {
             let role = res.data.role;
-            var users = res.data.users.results;
+            let users = res.data.users.results;
             if (users.length > 0 && searchedName != '') {
-                userListElement.innerHTML = getHtmlForListOfUsers(users, role);
-                displayNumbers.innerHTML = createPaginationButtons(res);
-                addActiveClass(res);
+                createUsersPageContentWithPagination(users, role, res)
             } else {
                 alertContent.innerHTML = addingAllertsAfterUserSearchsearchAllert(searchedName, users)
             }
@@ -327,7 +252,7 @@ function addingAllertsAfterUserSearchsearchAllert(searchedName, users) {
     return content;
 }
 
-// Pagination :
+
 
 let buttons = document.getElementsByClassName('page-link');
 function addActiveClassForButtons(res) {
@@ -343,32 +268,36 @@ function createUsersListWithPaginationHtml(element) {
     let pageNumber = element.value;
     let searchedName = document.getElementById('search_input').value;
     let status = document.getElementById('status').value;
-    var userListElement = document.getElementById('userList');
-    var displayNumbers = document.getElementById('pagination');
-    userListElement.innerHTML = '';
-    displayNumbers.innerHTML = '';
+    let userListElement = document.getElementById('userList');
+
     axios.get('http://localhost:3000/pagination', { params: { page: pageNumber, status: status, username: searchedName } })
         .then(res => {
-
             let role = res.data.role;
-            var users = res.data.users.results;
-            userListElement.innerHTML = getHtmlForListOfUsers(users, role);
-            displayNumbers.innerHTML = createPaginationButtons(res);
-            addActiveClassForButtons(res);
+            let users = res.data.users.results;;
+            createUsersPageContentWithPagination(users, role, res)
             document.getElementById('search_input').value = searchedName;
             if (userListElement.innerHTML == '') {
                 axios.get('http://localhost:3000/pagination', { params: { page: pageNumber - 1, status: status, username: searchedName } })
                     .then(res => {
-                        var users = res.data.users.results;
-                        userListElement.innerHTML = getHtmlForListOfUsers(users, role);
-                        displayNumbers.innerHTML = createPaginationButtons(res);
-                        addActiveClassForButtons(res);
+                        let users = res.data.users.results;
+                        let role = res.data.role;
+                        createUsersPageContentWithPagination(users, role, res)
                         document.getElementById('search_input').value = searchedName;
-                        console.log(document.getElementById('search_input').value);
                     })
             }
         })
         .catch(err => console.log(err))
+}
+
+
+function createUsersPageContentWithPagination(users, role, res) {
+    let userListElement = document.getElementById('userList');
+    userListElement.innerHTML = '';
+    let displayNumbers = document.getElementById('pagination');
+    displayNumbers.innerHTML = '';
+    userListElement.innerHTML = getHtmlForListOfUsers(users, role);
+    displayNumbers.innerHTML = createPaginationButtons(res);
+    addActiveClassForButtons(res);
 }
 
 function createPaginationButtons(res) {
